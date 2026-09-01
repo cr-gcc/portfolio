173a156f3ceb6eaf28267ccfe8ddabca4b42826d
Cris GC <cris.gcc@outlook.com>
2026-08-31 22:12:05 -0600

    Inicio del proyecto

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+    return (
+        <div className='flex h-screen items-center justify-center'>
+            <h1 className='text-3xl font-bold underline'>App</h1>
+        </div>
+    )
+}
+
+export default App
