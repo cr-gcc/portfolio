9c03a774aa2a6dafa15928d203ccb2b3cfb9e55b
Cris GC <cris.gcc@outlook.com>
2026-08-31 23:44:15 -0600

    Esqueleto base de home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,17 @@
+import { Hero } from "@sections/hero/Hero";
+import { Projects } from "@sections/projects/Projects";
+import { Experience } from "@sections/experience/Experience";
+import { TechStack } from "@sections/stack/TechStack";
+import { Contact } from "@sections/contact/Contact";
+
 export function HomePage() {
     return (
         <>
-            <div>Home Page</div>
+            <Hero />
+            <Projects />
+            <Experience />
+            <TechStack />
+            <Contact />
         </>
-    )
+    );
 }
---
+++ src/sections/stack/TechStack.tsx
@@ -0,0 +1,5 @@
+export function TechStack() {
+    return (
+        <div>TechStack</div>
+    )
+}       
